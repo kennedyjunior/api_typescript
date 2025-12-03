d018d0c73eefb2127a98e2a23198cb8cd1e48262
Kennedy Junior <josekennedy37@gmail.com>
2025-12-03 17:16:49 -0300

    at

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,13 +1,13 @@
-import { z } from "zod";
+import { optional, z } from "zod";
 
 export const createUserSchema = z.object({
   nome: z.string().min(1, "O nome é obrigatório"),
   idade: z.number().int().positive("A idade deve ser um número inteiro positivo"),
   email: z.string().email("Email inválido"),
-  telefone: z.string().min(8, "Telefone inválido"),
-  senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+  telefone: z.string().min(8, "Telefone inválido").optional(),
+  senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres").optional(),
   planoAluno: z.int().optional(),
-  treinadorResponsavelId: z.number().int().positive("ID do treinador responsável deve ser um número inteiro positivo")
+  treinadorResponsavelId: z.number().int().positive("ID do treinador responsável deve ser um número inteiro positivo").optional(),
 });
 
 export const updateUserSchema = z.object({
