5ad045efc977275ca6b3cdf344ade3b02a88a9c7
Kennedy Junior <josekennedy37@gmail.com>
2025-12-03 23:30:53 -0300

    essa eh a ultima

--- dist/config/jwt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtConfig = void 0;
-exports.jwtConfig = {
-    secret: process.env.JWT_SECRET || "qwerfghjkfASD2345dfgHJKLzxcvbnm!@#",
-    expiresIn: "1h",
-};
