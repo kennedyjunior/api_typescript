7dfec421f7e2307aa361265284591641c4bdd402
Kennedy Junior <josekennedy37@gmail.com>
2025-12-03 23:34:35 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app: Express = express();
 const port: number = 3000;
 
 app.use(cors({
-  origin: 'https://api-typescript-pi.vercel.app',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
